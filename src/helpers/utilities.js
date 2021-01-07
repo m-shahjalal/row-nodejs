ce2101b95fb71794d716de1ac08269b4572a8a7c
shahjalal <lx.shahjalal@gmail.com>
2021-01-07 11:45:28 +0600

    fouth chapter -Authentication completed

--- src/helpers/utilities.js
+++ src/helpers/utilities.js
@@ -1,28 +1,51 @@
 // dependencies
-const crypto = require('crypto');
-const environment = require('./environment');
+const crypto = require('crypto')
+const environment = require('./environment')
 
 // module scaffoldings
-const utilities = {};
+const utilities = {}
 
 // parsed JSON
 utilities.parsedJSON = (str) => {
-  let output;
-  try {
-    output = JSON.parse(str);
-  } catch (error) {
-    output = {};
-  }
-  return output;
-};
+	let output
+	try {
+		output = JSON.parse(str)
+	} catch (error) {
+		output = {}
+	}
+	return output
+}
 
 // hash string
 utilities.hash = (str) => {
-  if (typeof str === 'string' && str.length > 0) {
-    const hash = crypto.createHmac('sha256', environment.secretKey).update(str).digest('hex');
-    return hash;
-  }
-};
+	if (typeof str === 'string' && str.length > 0) {
+		const hash = crypto
+			.createHmac('sha256', environment.secretKey)
+			.update(str)
+			.digest('hex')
+		return hash
+	}
+}
+
+// token generator
+utilities.randomToken = (tokenLength) => {
+	const length = typeof tokenLength === 'number' ? tokenLength : false
+	if (length) {
+		let possibleCharacters =
+			'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+		let output = ''
+
+		for (let i = 0; i < length; i++) {
+			let chr = possibleCharacters.charAt(
+				Math.floor(Math.random() * possibleCharacters.length + 1)
+			)
+			output += chr
+		}
+		return output
+	} else {
+		return 'please give a number to the parameter'
+	}
+}
 
 // export modules
-module.exports = utilities;
+module.exports = utilities
--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 // dependencies
-const http = require('http');
-const { handleReqRes } = require('./helpers/handleReqRes');
-const environment = require('./helpers/environment');
-const data = require('./lib/data');
+const http = require('http')
+const { handleReqRes } = require('./helpers/handleReqRes')
+const environment = require('./helpers/environment')
+const data = require('./lib/data')
+const utilities = require('./helpers/utilities')
 // app object -module scaffolding
-const app = {};
+const app = {}
 
 // create server
 app.createServer = () => {
-  const server = http.createServer(app.handleReqRes);
-  server.listen(environment.port, () => console.log(`Server is listening at ${environment.port}`));
-};
+	const server = http.createServer(app.handleReqRes)
+	server.listen(environment.port, () =>
+		console.log(`Server is listening at ${environment.port}`)
+	)
+}
 
 // Handle request and response
 
-app.handleReqRes = handleReqRes;
+app.handleReqRes = handleReqRes
 
 // Start server
-app.createServer();
+app.createServer()
